a8b1f1c0289daa125ab3004187a8e667bcbd416b
AbdElRahman KahlOut <akahlouts@gmail.com>
2023-09-14 21:58:58 +0300

    adding stripe and payment form...

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,6 +2,7 @@ import {
   BaseButton,
   GoogleSignInButton,
   InvertedButton,
+  ButtonSpinner,
 } from "./Button.styles";
 
 export const BUTTON_TYPE_CLASSES = {
@@ -17,15 +18,16 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
 
   return (
     <CustomButton
+      disabled={isLoading}
       // className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
       {...otherProps}
     >
-      {children}
+      {isLoading ? <ButtonSpinner /> : children}
     </CustomButton>
   );
 };
